065b3347b8af3b940225d9c421cfc48843269ea2
root <root@raspberrypi.(none)>
2014-05-12 04:46:13 +0000

    Began HTML work

--- index.php
+++ index.php
@@ -5,3 +5,12 @@ $directory = "/home/pi/wac2/";
 file_put_contents($directory . "command","COMMAND");
 
 ?>
+
+<html>
+	<head>
+		<title>Wireless AC Control 1.4 BETA</title>
+	</head>
+	<body>
+		HELLO WORLD!
+	</body>
+</html>
